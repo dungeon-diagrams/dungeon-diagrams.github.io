141ba7cdbaeca318ffe5b3d586cb4abb6052cb2f
Jesse Clark <jjclark1982@gmail.com>
2022-07-28 16:58:57 -0700

    update tsconfig

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { h, Component, render } from "preact";
 import { sayHello } from "./widget.js";
 
 const el = document.getElementById("greeting");
-el.innerHTML = '';
-render(sayHello("here"), el);
+if (el) {
+    el.innerHTML = '';
+    render(sayHello("here"), el);
+}
