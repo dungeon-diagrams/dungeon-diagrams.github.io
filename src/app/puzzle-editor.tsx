c16c8670cf9f16cef8a37e94a372fc64fe1631bd
Jesse Clark <jjclark1982@gmail.com>
2022-08-14 22:56:12 -0700

    set numeric input type for mobile interface

--- src/app/puzzle-editor.tsx
+++ src/app/puzzle-editor.tsx
@@ -42,17 +42,17 @@ export class PuzzleEditor extends Component<PuzzleEditorProps, PuzzleEditorState
     render() {
         return (
             <div className="puzzle-editor">
-                <PuzzleGrid puzzle={this.state.puzzle} />
                 <div className="puzzle-editor-controls">
                     <input type="text" size={16} value={this.state.puzzle.name} onChange={this.setName}/>
                     {" "}
-                    <input type="number" size={1} value={this.state.puzzle.nCols} onChange={this.setCols}></input>
+                    <input type="number" inputMode="numeric" pattern="[0-9]*" size={1} value={this.state.puzzle.nCols} onChange={this.setCols}></input>
                     ✕
-                    <input type="number" size={1} value={this.state.puzzle.nRows} onChange={this.setRows}></input>
+                    <input type="number" inputMode="numeric" pattern="[0-9]*" size={1} value={this.state.puzzle.nRows} onChange={this.setRows}></input>
                     {" "}
                     <button>{this.state.monsterTile.emoji}</button>
                     <button>{this.state.bossTile.emoji}</button>
                 </div>
+                <PuzzleGrid puzzle={this.state.puzzle} />
             </div>
         )
     }
