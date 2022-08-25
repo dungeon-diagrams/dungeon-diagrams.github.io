536e016edb4ba5dae69f9819047273c1e795b545
Jesse Clark <jjclark1982@gmail.com>
2022-08-25 12:37:15 -0700

    use nunjucks to compile html templates

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,45 @@ import rewriteImports from "gulp-rewrite-imports";
 import sourcemaps from "gulp-sourcemaps";
 import ts from "gulp-typescript";
 import * as cjstoesm from "cjstoesm";
+import nunjucks from "gulp-nunjucks";
+import { readFile } from "node:fs/promises";
 
 const tsProject = ts.createProject("tsconfig.json");
 
 const paths = {
     static: [
-        "src/**/*.html",
         "src/**/*.css",
         "src/**/*.module.js",
-        "src/**/*.js.map"
+        "src/**/*.js.map",
+        "src/**/*.txt"
     ],
     tests: [
         "test/**/*"
+    ],
+    typescript: [
+        "src/**/*.ts",
+        "src/**/*.tsx"
+    ],
+    templates: [
+        "src/**/*.html"
     ],
     libsCJS: [
         "src/lib/runes.js"
     ]
 };
 
-gulp.task("copy-static", function () {
-    return (gulp.src(paths.static, { since: gulp.lastRun("copy-static") })
-        .pipe(gulp.dest("dist")));
+gulp.task("copy-static", function() {
+    return (
+        gulp.src(paths.static, { since: gulp.lastRun("copy-static") })
+        .pipe(gulp.dest("dist"))
+    );
 });
 
-gulp.task("watch-static", function () {
+gulp.task("watch-static", function() {
     gulp.watch(paths.static, gulp.parallel("copy-static"));
 });
 
-gulp.task("convert-cjs", async function (cb) {
+gulp.task("convert-cjs", async function(cb) {
     const result = await cjstoesm.transform({
         input: paths.libsCJS,
         outDir: "dist/lib/"
@@ -38,7 +49,20 @@ gulp.task("convert-cjs", async function (cb) {
     cb();
 });
 
-gulp.task("compile-typescript", function () {
+gulp.task("render-templates", async function(cb){
+    const packageJSON = JSON.parse(await readFile("./package.json", "utf8"));
+    return (
+        gulp.src(paths.templates)
+        .pipe(nunjucks.compile({package: packageJSON}))
+        .pipe(gulp.dest("./dist"))
+    );
+});
+
+gulp.task("watch-templates", function() {
+    gulp.watch(paths.templates.concat(["./package.json"]), gulp.series("render-templates"));
+});
+
+gulp.task("compile-typescript", function() {
     return (tsProject.src()
         .pipe(sourcemaps.init())
         .pipe(tsProject()).js
@@ -52,21 +76,21 @@ gulp.task("compile-typescript", function () {
         .pipe(gulp.dest("./dist")));
 });
 
-gulp.task('watch-typescript', function () {
-    gulp.watch(['src/**/*.ts', 'src/**/*.tsx'], { ignoreInitial: true }, gulp.series('compile-typescript'));
+gulp.task("watch-typescript", function() {
+    gulp.watch(paths.typescript, { ignoreInitial: true }, gulp.series("compile-typescript"));
 });
 
-gulp.task('build', gulp.parallel("copy-static", "convert-cjs", "compile-typescript"));
-
-gulp.task('build-test', function () {
+gulp.task("build-test", function() {
     return (gulp.src(paths.tests, { since: gulp.lastRun("build-test") })
         .pipe(gulp.dest("dist/test/")));
 });
 
-gulp.task("watch-test", function () {
+gulp.task("watch-test", function() {
     gulp.watch(paths.tests, gulp.parallel("build-test"));
 });
 
-gulp.task('watch', gulp.parallel('build', 'watch-static', 'watch-typescript', 'watch-test'));
+gulp.task("build", gulp.parallel("copy-static", "convert-cjs", "render-templates", "compile-typescript"));
 
-gulp.task('default', gulp.series('build'));
+gulp.task("watch", gulp.parallel("build", "watch-static", "watch-templates", "watch-typescript", "watch-test"));
+
+gulp.task("default", gulp.series("build"));
