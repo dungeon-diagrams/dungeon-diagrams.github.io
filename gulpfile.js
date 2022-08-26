e49c59c524edb942ae3653dc2437f5d8d1ee23d4
Jesse Clark <jjclark1982@gmail.com>
2022-08-26 12:10:19 -0700

    add task descriptions to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ import ts from "gulp-typescript";
 import * as cjstoesm from "cjstoesm";
 import nunjucks from "gulp-nunjucks";
 import { readFile } from "node:fs/promises";
-import rename from 'gulp-rename';
+import rename from "gulp-rename";
+import compile from "gulp-typescript";
 
 const tsProject = ts.createProject("tsconfig.json");
 
@@ -32,74 +33,120 @@ const paths = {
     ]
 };
 
-gulp.task("copy-static", function() {
+export function copyStatic(cb) {
     return (
-        gulp.src(paths.static, { since: gulp.lastRun("copy-static") })
+        gulp.src(paths.static, { since: gulp.lastRun(copyStatic) })
         .pipe(gulp.dest("dist"))
     );
-});
+}
+copyStatic.displayName = "copy-static";
+copyStatic.description = "Copy static files (css, robots.txt, vendor libraries)"
+gulp.task(copyStatic);
 
-gulp.task("watch-static", function() {
-    gulp.watch(paths.static, gulp.parallel("copy-static"));
-});
+export function watchStatic() {
+    gulp.watch(paths.static, {ignoreInitial: false}, copyStatic);
+}
+watchStatic.displayName = "watch-static";
+gulp.task(watchStatic);
 
-gulp.task("convert-cjs", async function(cb) {
+export async function convertCJS(cb) {
     const result = await cjstoesm.transform({
         input: paths.libsCJS,
         outDir: "dist/lib/"
     });
     cb();
-});
+}
+convertCJS.displayName = "convert-cjs";
+convertCJS.description = "Convert vendor libraries from CommonJS to ES modules";
+gulp.task(convertCJS);
 
-gulp.task("render-templates", async function(cb){
+export async function renderTemplates(cb) {
     const packageJSON = JSON.parse(await readFile("./package.json", "utf8"));
     const context = {package: packageJSON};
     return (
         gulp.src(paths.templates)
         .pipe(nunjucks.compile(context))
         .pipe(rename(function(path){
-            if (path.basename === 'manifest') {
-                path.extname = '.webmanifest';
+            if (path.basename === "manifest") {
+                path.extname = ".webmanifest";
             }
             return path;
         }))
         .pipe(gulp.dest("./dist"))
     );
-});
+}
+renderTemplates.displayName = "render-templates";
+renderTemplates.description = "Fill HTML files with values from package.json";
+gulp.task(renderTemplates);
 
-gulp.task("watch-templates", function() {
-    gulp.watch(paths.templates.concat(["./package.json"]), gulp.series("render-templates"));
-});
+export function watchTemplates() {
+    gulp.watch(paths.templates.concat(["./package.json"]), {ignoreInitial: false}, renderTemplates);
+}
+watchTemplates.displayName = "watch-templates";
+gulp.task(watchTemplates);
 
-gulp.task("compile-typescript", function() {
-    return (tsProject.src()
+export function compileTypescript() {
+    return (
+        tsProject.src()
         .pipe(sourcemaps.init())
         .pipe(tsProject()).js
         .pipe(rewriteImports({
-        mappings: {
-            'preact': '../lib/preact.module.js',
-            'runes': '../lib/runes.js'
-        }
-    }))
+            mappings: {
+                "preact": "../lib/preact.module.js",
+                "runes": "../lib/runes.js"
+            }
+        }))
         .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest("./dist")));
-});
+        .pipe(gulp.dest("./dist"))
+    );
+}
+compileTypescript.displayName = "compile-typescript";
+compileTypescript.description = "Compile TypeScript source to JavaScript"
+gulp.task(compileTypescript);
 
-gulp.task("watch-typescript", function() {
-    gulp.watch(paths.typescript, { ignoreInitial: true }, gulp.series("compile-typescript"));
-});
+export function watchTypescript() {
+    gulp.watch(paths.typescript, { ignoreInitial: false }, compileTypescript);
+}
+watchTypescript.displayName = "watch-typescript";
+gulp.task(watchTypescript);
 
-gulp.task("build-test", function() {
-    return (gulp.src(paths.tests, { since: gulp.lastRun("build-test") })
-        .pipe(gulp.dest("dist/test/")));
-});
+export function buildTest(cb) {
+    return (
+        gulp.src(paths.tests, { since: gulp.lastRun(buildTest) })
+        .pipe(gulp.dest("dist/test/"))
+    );
+}
+buildTest.displayName = "build-test";
+buildTest.description = "Copy test code to dist for running in a browser";
+gulp.task(buildTest);
 
-gulp.task("watch-test", function() {
-    gulp.watch(paths.tests, gulp.parallel("build-test"));
-});
+export function watchTest() {
+    gulp.watch(paths.tests, { ignoreInitial: false }, buildTest);
+}
+watchTest.displayName = "watch-test";
+gulp.task(watchTest);
 
-gulp.task("build", gulp.parallel("copy-static", "convert-cjs", "render-templates", "compile-typescript"));
+export const build = gulp.parallel(
+    copyStatic,
+    convertCJS,
+    renderTemplates,
+    compileTypescript
+);
+build.displayName = "build";
+build.description = "Build the distribution (except tests)";
+gulp.task(build);
 
-gulp.task("watch", gulp.parallel("build", "watch-static", "watch-templates", "watch-typescript", "watch-test"));
+export const watch = gulp.parallel(
+    convertCJS,
+    watchTypescript,
+    watchStatic,
+    watchTemplates,
+    watchTest
+);
+watch.displayName = "watch";
+watch.description = "Continuously rebuild files when they change";
 
-gulp.task("default", gulp.series("build"));
+// gulp.task("default", function(cb){
+//     console.log(gulp.tree());
+//     cb();
+// });
