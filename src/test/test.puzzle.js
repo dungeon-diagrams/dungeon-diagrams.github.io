a70e3399754dcf165e3a6ffd5649fb687bd1604c
Jesse Clark <jjclark1982@gmail.com>
2022-08-29 13:52:06 -0700

    failing test for connected halls

--- src/test/test.puzzle.js
+++ src/test/test.puzzle.js
@@ -4,17 +4,32 @@ import * as PuzzleString from "../app/puzzle-string.js";
 describe("Puzzle Model", ()=>{
     it("should detect a completely unsolved puzzle", function(){
         const puzzle = PuzzleString.parse("Test Puzzle 1!.25332332!1!4!2!2...👑..🐀!3!4.....🐍!2!5");
-        const solved = puzzle.isSolved().solved;
+        const {solved, reason} = puzzle.isSolved();
         if (solved) {
-            throw new Error("incorrectly thought an unsolved puzzle was solved");
+            throw new Error(`incorrectly thought an unsolved puzzle was solved: ${reason}`);
         }
     });
 
     it("should detect a correctly solved puzzle", function(){
         const puzzle = PuzzleString.parse("Example Dungeon!.424121!3***..💎!1*!2*.*!5*.****!1....*🐲!2🦁**");
-        const solved = puzzle.isSolved().solved;
+        const {solved, reason} = puzzle.isSolved();
         if (!solved) {
-            throw new Error("incorrectly thought a solved puzzle was unsolved");
+            throw new Error(`incorrectly thought a solved puzzle was unsolved: ${reason}`);
+        }
+    });
+
+    it("should detect disconnected halls", function(){
+        const puzzle = PuzzleString.parse(
+            `Test Puzzle
+            .030
+            1m*m
+            1.*.
+            1m*m
+            `
+        );
+        const {solved, reason} = puzzle.isSolved();
+        if (solved) {
+            throw new Error(`incorrectly thought disconnected halls were solved: ${reason}`);
         }
     });
 });
